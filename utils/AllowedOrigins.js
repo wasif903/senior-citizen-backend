5fbcc60dc0ef59b3dc2def3878232e4e99d75a6c
wasif903 <wasifmehmood903@gmail.com>
2025-11-27 05:33:12 +0500

    Updating Changes

--- utils/AllowedOrigins.js
+++ utils/AllowedOrigins.js
@@ -2,5 +2,5 @@ export const allowedOrigins = [
     "http://localhost:3000",
     "http://31.97.141.173:3000",
     "http://www.justseniors.org",
-    "http://justseniors.org/"
+    "http://justseniors.org"
 ]
