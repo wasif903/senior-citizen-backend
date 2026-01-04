6fb2077d46538ef58d8453a51d8911faabf9649e
wasif903 <wasifmehmood903@gmail.com>
2026-01-05 03:37:09 +0500

    Fix

--- middlewares/RateLimiter.js
+++ middlewares/RateLimiter.js
@@ -1,11 +1,10 @@
 import rateLimit from "express-rate-limit";
 
 const RateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 15 * 60 * 1000,
   max: 100,
   standardHeaders: true,
   legacyHeaders: false,
-  // trustProxy is NOT a valid option here - remove it
 });
 
 export default RateLimiter;
