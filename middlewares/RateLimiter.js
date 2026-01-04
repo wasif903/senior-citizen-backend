de43cd40f4bc389d303b7e6cc8e24d029008c667
wasif903 <wasifmehmood903@gmail.com>
2026-01-05 03:29:07 +0500

    Fix

--- middlewares/RateLimiter.js
+++ middlewares/RateLimiter.js
@@ -1,11 +1,11 @@
-import rateLimit, { ipKeyGenerator } from "express-rate-limit";
+import rateLimit from "express-rate-limit";
 
 const RateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100,
   standardHeaders: true,
   legacyHeaders: false,
-  keyGenerator: (req) => ipKeyGenerator(req), // IPv6 safe
+  // keyGenerator: (req) => ipKeyGenerator(req), // IPv6 safe
   trustProxy: true, // ✅ allows keyGenerator to read real client IP behind proxy
 });
 
