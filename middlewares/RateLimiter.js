40cdbd39fcf2303c1ba2aaee1132bddd09bf9ede
wasif903 <wasifmehmood903@gmail.com>
2026-01-05 03:16:01 +0500

    Fix

--- middlewares/RateLimiter.js
+++ middlewares/RateLimiter.js
@@ -1,18 +1,14 @@
-import rateLimit, { ipKeyGenerator } from "express-rate-limit";
+import rateLimit from "express-rate-limit";
 
 const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 15 * 60 * 1000,
   max: 100,
   standardHeaders: true,
   legacyHeaders: false,
 
-  // 🔥 Disable proxy header validation (you are behind Nginx)
   validate: {
     xForwardedForHeader: false,
   },
-
-  // ✅ IPv4 + IPv6 safe
-  keyGenerator: (req, res) => ipKeyGenerator(req, res),
 });
 
 export default rateLimiter;
